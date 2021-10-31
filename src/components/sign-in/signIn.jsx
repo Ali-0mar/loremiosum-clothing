dbe22094096af8c1e119a77abee3d9361a4a431c
Ali Omar <ali95.muhom@gmail.com>
2021-10-31 16:41:45 +0200

    Fix the bug  in the sign in component

--- src/components/sign-in/signIn.jsx
+++ src/components/sign-in/signIn.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../sign-in/sign-in.scss";
 import FormInput from "../Form/Form";
 import CustomtButton from "../customButton/customButton";
-import { signInWithGoogle } from "../../firebase/firebase";
+import { auth, signInWithGoogle } from "../../firebase/firebase";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -13,9 +13,15 @@ class SignIn extends React.Component {
             password: "",
         };
     }
-    handleSubmit = (event) => {
+    handleSubmit = async (event) => {
         event.preventDefault();
-        this.setState({ email: "", password: "" });
+        const { email, password } = this.state;
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: "", password: "" });
+        } catch (error) {
+            console.log(error);
+        }
     };
     handleChange = (event) => {
         const { value, name } = event.target;
@@ -38,7 +44,7 @@ class SignIn extends React.Component {
                     <FormInput
                         name="password"
                         type="password"
-                        value={this.state.email}
+                        value={this.state.password}
                         required
                         handleChange={this.handleChange}
                         label="password"
@@ -56,40 +62,5 @@ class SignIn extends React.Component {
         );
     }
 }
-
-/*
-for(let x=0;i<3;i++){
-    for(let y=0;j<6;j++) {
-        switch a[1][5]{
-            case a[i][j-1]  a[i][j+1] ()
-
-
-
-            a[i][j
-            a[i][j]
-            icludes((1,0) (0,1))
-        }
-        }
-    }
-}
-
-3
-a = [
-    b=[1,0,0,1,0,0],=a[0][0]
-  c=  [1,43543,3453,435],a[1]
-    d=[,5,6,6,6,6,6,],a[2]
-]
-lowestCost = 0;
-if(from a to b <lowestCost) {
-    lowestCase= a to d
-}
-let myArray = [] myArray[0]
-for  {cost 3} .push
-
-
-a , b, c, d, f
-a[0]=b
-b[0]
-*/
 
 export default SignIn;
