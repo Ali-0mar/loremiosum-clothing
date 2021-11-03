a9bf64b37b419eddbea53e82af147d9ecbd528b3
Ali Omar <ali95.muhom@gmail.com>
2021-11-03 15:26:49 +0200

    Completing the Checkout page

--- src/redux/cart/cart-types.js
+++ src/redux/cart/cart-types.js
@@ -1,6 +1,8 @@
 const CartTypes = {
     TOOGLE_CART_HIDDEN: "TOOGLE_CART_HIDDEN",
     ADD_ITEM: "ADD_ITEM",
+    REMOVE_ITEM: "REMOE_VITEM",
+    CLEAR_ITEM_FROM_CART: "CLEAR_ITEM_FROM_CART ",
 };
 
 export default CartTypes;
